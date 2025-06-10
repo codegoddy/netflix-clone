621c62a8356544eb7d9a9bb8dc6600ac826921af
Godwin Mayodi <godwin.mayodi@student.moringaschool.com>
2025-06-10 20:12:31 +0300

    feat: implement Home, Navbar, Footer, and TitleCards components with styling; replace favicon and update index.html title

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useRef} from 'react';
+import './TitleCards.css';
+import cards_data from '../../assets/cards/Cards_data.js';
+
+
+const TitleCards = () => {
+
+    const cardsRef = useRef();
+
+    useEffect(() => {
+        cardsRef.current.addEventListener('wheel',handleWheel)
+    }, []);
+
+    const handleWheel = (e) => {
+        e.preventDefault();
+        cardsRef.current.scrollLeft += e.deltaY;
+    }
+
+    return (
+        <div className='title-cards'>
+            <h2>
+                Popular on Netflix
+            </h2>
+            <div className='card-list' ref={cardsRef}>
+                {cards_data.map((card,index)=>{
+                    return <div className='card' key={index}>
+                        <img src={card.image} alt=''/>
+                        <p>{card.name}</p>
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default TitleCards;
